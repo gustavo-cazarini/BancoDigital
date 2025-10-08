899fbf43d4c04356310bdeb011202b51c89d2103
Gustavo Cazarini <gustavo.cazarini@outlook.com>
2025-10-08 00:42:54 -0300

    feat: modelo e

---
+++ BancoDigital.Api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BancoDigital.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BancoDigital.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<ContaModel> Contas { get; set; }
+    }
+}
---
+++ BancoDigital.Api/Models/ContaModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BancoDigital.Api.Models
+{
+    public class ContaModel
+    {
+        [Key]
+        [Required]
+        [MinLength(1)]
+        public int Conta { get; set; }
+        public decimal Saldo { get; set; }
+    }
+}
