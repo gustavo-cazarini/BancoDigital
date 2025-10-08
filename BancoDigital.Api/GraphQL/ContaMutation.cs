47d0329bb0900f086283c3ad1092b489c6b0ca92
Gustavo Cazarini <gustavo.cazarini@outlook.com>
2025-10-08 00:46:00 -0300

    feat: 'rotas' do graphql

---
+++ BancoDigital.Api/GraphQL/ContaMutation.cs
@@ -0,0 +1,25 @@
+﻿using BancoDigital.Api.Models;
+using BancoDigital.Api.Services;
+
+namespace BancoDigital.Api.GraphQL
+{
+    public class ContaMutation
+    {
+        private readonly ContaService _service;
+
+        public ContaMutation(ContaService service)
+        {
+            _service = service;
+        }
+
+        public async Task<ContaModel> Sacar(int conta, decimal valor)
+        {
+            return await _service.SacarAsync(conta, valor);
+        }
+
+        public async Task<ContaModel> Depositar(int conta, decimal valor)
+        {
+            return await _service.DepositarAsync(conta, valor);
+        }
+    }
+}
---
+++ BancoDigital.Api/GraphQL/ContaQuery.cs
@@ -0,0 +1,19 @@
+﻿using BancoDigital.Api.Services;
+
+namespace BancoDigital.Api.GraphQL
+{
+    public class ContaQuery
+    {
+        private readonly ContaService _service;
+
+        public ContaQuery(ContaService service)
+        {
+            _service = service;
+        }
+
+        public async Task<decimal> Saldo(int conta)
+        {
+            return await _service.ConsultarSaldoAsync(conta);
+        }
+    }
+}
